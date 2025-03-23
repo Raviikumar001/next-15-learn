c15ad3395021ea7ead2cbf44995a68c030cd93ca
Ravi <samcollins9899@gmail.com>
2025-03-23 13:27:05 +0530

    feat: added navigation link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <h2> hi</h2>
+      <h2> hi! Welcome</h2>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">products </Link>
     </>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
 
 export default function Products() {
-    return (
-        <div >
-            <h1>Products</h1>
-        </div>
-    );
-    }
+  return (
+    <div>
+      <Link href="/"> Home</Link>
+      <h1>Products</h1>``
+      <h2>
+        <Link href="products/1"> Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2"> Product 2</Link>
+      </h2>
+    </div>
+  );
+}
