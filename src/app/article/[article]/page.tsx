042014d7806a470a395b2638204b83214961c248
Ravi <samcollins9899@gmail.com>
2025-04-26 23:40:47 +0530

    feat: made files to use search params

---
+++ src/app/article/[article]/page.tsx
@@ -0,0 +1,8 @@
+export default function NewsArticle() {
+  return (
+    <div>
+      <h1>News article id</h1>
+      <p>hh</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       <h2> hi! Welcome</h2>
       <Link href="/blog">Blog</Link>
       <Link href="/products">products </Link>
+      <Link href="/article/breaking-news-123?lang=en">Read in English</Link>
+      <Link href="/article/breaking-news-123?lang=fr">Read in French</Link>
     </>
   );
 }
