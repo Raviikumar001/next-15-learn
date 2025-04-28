f569e7744c47ced43710e68dce0acc4da762bef7
Ravi <samcollins9899@gmail.com>
2025-04-28 19:14:19 +0530

    feat: added linking and navigation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,18 +1,22 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
-
-export  default async function ProdocutReview({params,}: {
-    params: Promise<{productId: string, reviewId: string}>;
+export default async function ProdocutReview({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
 }) {
-    const {productId, reviewId} = await params;
+  const { productId, reviewId } = await params;
 
-    if(parseInt(reviewId) > 1000) {
-        notFound();
-    }
+  if (parseInt(reviewId) > 1000) {
+    // notFound();
+    redirect("/products");
+  }
 
-    return (
-        <div>
-            <h1>Product Review {productId} {reviewId}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>
+        Product Review {productId} {reviewId}
+      </h1>
+    </div>
+  );
 }
