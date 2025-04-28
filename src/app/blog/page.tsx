759d80a6fc8f7d0e8a79fc3ab86c86f29d82c3b2
Ravi <samcollins9899@gmail.com>
2025-04-28 20:47:16 +0530

    feat: learnt about loading files

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,12 @@
-
-export default function Blog() {
-    return (
-        <div >
-            <h1>Blog</h1>
-        </div>
-    );
-    }
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("intential delay");
+    }, 2000);
+  });
+  return (
+    <div>
+      <h1>Blog</h1>
+    </div>
+  );
+}
